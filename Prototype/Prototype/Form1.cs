e138c7a54b0ae8be31bae984a72c135ea1c32bc8
Sasha <117905137+Ghostinchik@users.noreply.github.com>
2023-04-03 16:53:14 +0300

    Prototype

---
+++ Prototype/Prototype/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Prototype;
+using static Prototype.Prototype;
+
+namespace Prototype
+{
+    public partial class Form1 : Form
+    {
+        private Prototype.Bread _bread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Prototype.BreadBuilder breadBuilder = new Prototype.BreadBuilder(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text);
+            _bread = new Prototype.Bread(breadBuilder);
+
+            MessageBox.Show("Успішне створення хліба");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_bread == null)
+            {
+                MessageBox.Show("Будь ласка, створіть хліб перед клонуванням");
+            }
+            else
+            {
+                Prototype.Bread clonedBread = _bread.Clone();
+
+                textBox1.Text = clonedBread.Flour;
+                textBox2.Text = clonedBread.Water;
+                textBox3.Text = clonedBread.Yeast;
+                textBox4.Text = clonedBread.Salt;
+                textBox5.Text = clonedBread.Sugar;
+
+                MessageBox.Show("Хліб успішно клоновано");
+            }
+
+        }
+    }
+}
---
+++ Prototype/Prototype/Prototype.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototype
+{
+    internal class Prototype
+    {
+        public class BreadBuilder
+        {
+            public string Flour { get; set; }
+            public string Water { get; set; }
+            public string Yeast { get; set; }
+            public string Salt { get; set; }
+            public string Sugar { get; set; }
+
+            public BreadBuilder(string flour, string water, string yeast, string salt, string sugar)
+            {
+                Flour = flour;
+                Water = water;
+                Yeast = yeast;
+                Salt = salt;
+                Sugar = sugar;
+            }
+
+            public BreadBuilder Clone()
+            {
+                return (BreadBuilder)this.MemberwiseClone();
+            }
+        }
+        public class Bread
+        {
+            public string Flour { get; set; }
+            public string Water { get; set; }
+            public string Yeast { get; set; }
+            public string Salt { get; set; }
+            public string Sugar { get; set; }
+
+            public Bread(BreadBuilder builder)
+            {
+                Flour = builder.Flour;
+                Water = builder.Water;
+                Yeast = builder.Yeast;
+                Salt = builder.Salt;
+                Sugar = builder.Sugar;
+            }
+
+            public Bread Clone()
+            {
+                return (Bread)this.MemberwiseClone();
+            }
+        }
+
+    }
+}
